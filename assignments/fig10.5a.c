3fc5926bb64087a72e0e4cbee493c447204a3d9f
Michael Kirkedal Thomsen <kirkedal@acm.org>
2019-09-11 11:16:34 +0200

    Code from figures in 10

---
+++ assignments/fig10.5a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void hello(char *tag) {
+  char inp[16];
+  printf("Enter value for %s: ", tag); gets(inp);
+  printf("Hello your %s is %s\n", tag, inp);
+}
+
+int main(int argc, char *argv[]) {
+  hello(argv[1]);
+}
---
+++ assignments/fig10.7a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void getinp(char *inp, int siz) {
+  puts("Input value: ");
+  fgets(inp, siz, stdin);
+  printf("buffer3 getinp read %s\n", inp);
+}
+
+void display(char *val) {
+  char tmp[16];
+  sprintf(tmp, "read val: %s\n", val); puts(tmp);
+}
+
+int main(int argc, char *argv[]) {
+  char buf[16];
+  getinp(buf, sizeof(buf)); display(buf); printf("buffer3 done\n");
+}
