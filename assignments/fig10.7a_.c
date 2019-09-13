dba8318b8a616e8bff9f08a81b7873cce94fccc3
Michael Kirkedal Thomsen <kirkedal@acm.org>
2019-09-13 09:10:36 +0200

    En fil for meget

--- assignments/fig10.7a_.c
+++ assignments/fig10.7a_.c
@@ -8,10 +8,13 @@ void getinp(char *inp, int siz) {
 
 void display(char *val) {
   char tmp[16];
-  sprintf(tmp, "read val: %s\n", val); puts(tmp);
+  sprintf(tmp, "read val: %s\n", val);
+  puts(tmp);
 }
 
 int main(int argc, char *argv[]) {
   char buf[16];
-  getinp(buf, sizeof(buf)); display(buf); printf("buffer3 done\n");
+  getinp(buf, sizeof(buf));
+  display(buf);
+  printf("buffer3 done\n");
 }
